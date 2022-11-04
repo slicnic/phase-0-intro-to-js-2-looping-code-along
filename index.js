d2b3a1b244cd14890f9b36a76718720109e02f8d
Nic <115425395+slicnic@users.noreply.github.com>
2022-11-04 11:54:04 -0600

    commit tree 05f3b53f7ac6b92b9ae9b29ce00a3b20fa854976

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}*/
+
+//wrapGifts(gifts);
+
+//const peopleReceivingCards = ["Charlie", "Samip", "Ali"];
+//const forEvent = "surprise";
+//const people = ['Guadalupe', 'Samip', 'Ali'];
+
+function writeCards(people, forEvent){
+  const array = []
+    for(let i = 0; i<people.length; i++){
+        array.push(`Thank you, ${people[i]}, for the wonderful ${forEvent} gift!`)
+    }
+    return array 
+}
+//console.log(writeCards(people, forEvent));
+
+function countDown (num){
+  while (num>=0){
+    console.log(num)
+    num--
+  }
+}
+
+countDown(10)
